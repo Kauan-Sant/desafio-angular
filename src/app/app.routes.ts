b849cd0a16f7a76cb9a950da151b885ccb1d68f5
Kauan Sant <09kauanjesus@gmail.com>
2025-05-02 16:57:48 -0300

    estilização quase "pronto"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'dashboard', component: DashboardComponent},
+];
