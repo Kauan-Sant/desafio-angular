fe31a4d86501a4cc1739a64700ddc267390e4f07
ksant <09kauanjesus@gmail.com>
2025-05-05 10:46:26 -0300

    Estilização

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
+    {path: '', component: LoginComponent},
     {path: 'login', component: LoginComponent},
     {path: 'home', component: HomeComponent},
     {path: 'dashboard', component: DashboardComponent},
