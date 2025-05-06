aa9652f37c799db12fce85a92b6c45856d2d6a41
Kauan Sant <09kauanjesus@gmail.com>
2025-05-06 16:45:15 -0300

    add api

---
+++ src/app/components/dashboard/caracts/caracts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caracts',
+  standalone: true,
+  imports: [],
+  templateUrl: './caracts.component.html',
+  styleUrl: './caracts.component.scss'
+})
+export class CaractsComponent {
+
+}
